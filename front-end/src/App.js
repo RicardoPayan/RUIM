b48ca84e706189ba03360c48d7e58e825048a4e5
Fermin A. Ahumada Garcia <a220207901@unison.mx>
2022-10-24 16:50:07 -0700

    puff

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,6 +4,11 @@ import Sidebar from './components/Sidebar';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Inicio from './components/Pages/Inicio';
 import Ubicacion from './components/Pages/Ubicacion';
+import Calendario from './components/Pages/Calendario';
+import Avisos from './components/Pages/Avisos';
+import Memorias from './components/Pages/Memorias';
+import Organizacion from './components/Pages/Organizacion';
+import Plantilla from './components/Pages/Plantilla';
 
 
 function App() {
@@ -14,6 +19,11 @@ function App() {
         <Routes>
           <Route path="/" element={<Inicio/>}/>
           <Route path="/ubicacion" element={<Ubicacion/>}/>
+          <Route path="/calendario" element={<Calendario/>}/>
+          <Route path="/avisos" element={<Avisos/>}/>
+          <Route path="/memorias" element={<Memorias/>}/>
+          <Route path="/organizacion" element={<Organizacion/>}/>
+          <Route path="/plantilla" element={<Plantilla/>}/>
         </Routes>
       </Sidebar>
     </BrowserRouter>
