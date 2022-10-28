0e2bc12831cdeacc0fa63f87194fcb7058e13ec2
RicardoPayan <a218206581@unison.mx>
2022-10-28 16:09:21 -0700

    Continuar despues

--- frontend/src/paginas/Inicio.jsx
+++ frontend/src/paginas/Inicio.jsx
@@ -1,4 +1,20 @@
+import {useState, useEffect} from "react";
+import axios from "axios"
+
 const Inicio = () =>{
+  
+  //Solicitando cuerpo de la paginas desde el servidor
+  
+     useEffect(async () => {
+      try{
+        const url = "http://localhost:4000/api/ruimMain/"
+        const cuerpoPagina = await axios(url)
+      }catch(error){
+        console.log(error)
+      }
+   }, []);
+   
+ 
   return(
     <>
       <h1>Desde Inicio </h1>
