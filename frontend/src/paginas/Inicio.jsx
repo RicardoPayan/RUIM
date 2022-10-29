42dc3c948eae121a9f8927a2cf03ccf7f5757777
RicardoPayan <a218206581@unison.mx>
2022-10-29 15:07:40 -0700

    Conectando front con back exitosamente

--- frontend/src/paginas/Inicio.jsx
+++ frontend/src/paginas/Inicio.jsx
@@ -1,23 +1,44 @@
-import {useState, useEffect} from "react";
-import axios from "axios"
+import {useState, useEffect, createContext} from "react";
+import clienteAxios from "../../config/axios.jsx";
+import data from "bootstrap/js/src/dom/data.js";
 
 const Inicio = () =>{
   
   //Solicitando cuerpo de la paginas desde el servidor
-  
-     useEffect(async () => {
-      try{
-        const url = "http://localhost:4000/api/ruimMain/"
-        const cuerpoPagina = await axios(url)
-      }catch(error){
-        console.log(error)
-      }
-   }, []);
-   
- 
+
+    const [cuerpoPagina, setCuerpoPagina] = useState({});
+
+    useEffect(() =>{
+        const obtenerCuerpoPagina = async  () =>{
+
+            try {
+                const config = {
+                    headers: {
+                        "Content-Type" : "application/json"
+                    }
+                }
+                const {data} = await clienteAxios('/ruimMain', config);
+                console.log(data)
+                setCuerpoPagina(data);
+
+            }catch (error){
+                console.log(error);
+            }
+
+        }
+        obtenerCuerpoPagina();
+    }, [])
+
+    const {nombreEvento, fechas,lugar, parrafo1, parrafo2} = cuerpoPagina;
+    console.log(nombreEvento);
+
   return(
     <>
-      <h1>Desde Inicio </h1>
+      <h1> {nombreEvento}</h1>
+      <p>{fechas}</p>
+      <p>{lugar}</p>
+      <p>{parrafo1}</p>
+      <p>{parrafo2}</p>
     </>
   ) 
 }
